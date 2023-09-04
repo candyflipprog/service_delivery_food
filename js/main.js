30194a6663d0c8eb9a12ca91523bb056ac61a216
candyflipprog <dmitriymarlowpierrebourne@gmail.com>
2023-09-05 00:15:00 +0300

    Main fixes commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+import { renderingDiners } from "./renderingDiners.js";
+import { searchDiners } from "./searchDiners.js";
+document.addEventListener("DOMContentLoaded", async () => {
+    renderingDiners();    
+    searchDiners();
+});
---
+++ js/renderingDiners.js
@@ -0,0 +1,62 @@
+export async function renderingDiners() {
+    fetch("../db/db.json").then((response) => response.json()).then(
+        (json) => {
+            const container = document.querySelector(".cards-restaurants");
+            for(let elem of json.db.partners) {
+                const itemURL = document.createElement("a");
+                itemURL.href = `restaurant.html`;
+                itemURL.classList.add("card");
+                itemURL.classList.add("card-restaurant");
+
+                const itemIMG = document.createElement("img");
+                itemIMG.alt = "image";
+                itemIMG.classList.add("card-image");
+                itemIMG.src = `${elem.image}`
+
+                const itemCardText = document.createElement("div");
+                itemCardText.classList.add("card-text");
+
+                const itemCardHeading = document.createElement("div");
+                itemCardHeading.classList.add("card-heading");
+
+                const itemTitle = document.createElement("h3");
+                itemTitle.classList.add("card-title");
+                itemTitle.textContent = `${elem.name}`;
+
+                const itemTimeDelivery = document.createElement("span");
+                itemTimeDelivery.classList.add("card-tag");
+                itemTimeDelivery.classList.add("tag");
+                itemTimeDelivery.textContent = `${elem.time_of_delivery}`;
+                
+                const itemCardInfo = document.createElement("div");
+                itemCardInfo.classList.add("card-info");
+
+                const itemCardRating = document.createElement("div");
+                itemCardRating.classList.add("rating");
+                itemCardRating.textContent = `${elem.stars}`;
+
+                const itemCardPrice = document.createElement("div");
+                itemCardPrice.classList.add("price");
+                itemCardPrice.textContent = `${elem.price}`;
+
+                const itemCardCategory = document.createElement("div");
+                itemCardCategory.classList.add("category");
+                itemCardCategory.textContent = `${elem.kitchen}`;
+
+                container.append(itemURL, itemIMG);
+                
+                itemCardHeading.append(itemTitle, itemTimeDelivery);
+
+                itemCardText.append(itemCardHeading);
+
+                itemCardInfo.append(itemCardRating, itemCardPrice, itemCardCategory);
+
+                itemCardText.append(itemCardInfo);
+                
+                itemURL.append(itemIMG, itemCardText);
+
+                container.append(itemURL);
+            }
+        }
+    );
+}
